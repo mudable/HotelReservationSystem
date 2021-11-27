1917b75374b9c4da197df4e18a4fc884c7bb710a
mudable <mudableshilpa@gmail.com>
2021-11-27 11:56:40 +0530

    Added : UC3 to calculate weekEnd and WeekDay

--- src/main/java/com/bridgelabz/hotelreservationstsyem/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/hotelreservationstsyem/HotelReservationSystem.java
@@ -10,79 +10,72 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class HotelReservationSystem
-{
+public class HotelReservationSystem {
 
-    private List<Hotel> hotels;
+	private List<Hotel> hotels;
 
-    public HotelReservationSystem()
-    {
-        this.hotels = new ArrayList<Hotel>();
-    }
+	public HotelReservationSystem() {
+		this.hotels = new ArrayList<Hotel>();
+	}
 
-    public void add(Hotel hotel) // add hotel.
-    {
-        this.hotels.add(hotel);
-    }
+	public void add(Hotel hotel) // add hotel.
+	{
+		this.hotels.add(hotel);
+	}
 
-    public List<Hotel> getHotelList()
-    {
-        return this.hotels;
-    }
+	public List<Hotel> getHotelList() {
+		return this.hotels;
+	}
 
-    public Map<Integer, Hotel> searchFor(String date1, String date2)
-    {
-        int totalDays = countTotalDays(date1, date2);
-        int weekDays = countWeekDays(date1, date2);
-        int weekendDays = totalDays - weekDays;
-        return getCheapestHotels(weekDays, weekendDays);
-    }
+	public Map<Integer, Hotel> searchFor(String date1, String date2) {
+		int totalDays = countTotalDays(date1, date2);
+		int weekDays = countWeekDays(date1, date2);
+		int weekendDays = totalDays - weekDays;
+		return getCheapestHotels(weekDays, weekendDays);
+	}
 
-    public Map<Integer, Hotel> getCheapestHotels(int weekDays, int weekendDays)
-    {
-        Map<Integer, Hotel> hotelCosts = new HashMap<>();
-        Map<Integer, Hotel> sortedHotelCosts = new HashMap<>();
-        if (hotels.size() == 0)
-            return null;
-        this.hotels.stream().forEach(
-                n -> hotelCosts.put(n.getRegularWeekdayRate() * weekDays + n.getRegularWeekendRate() * weekendDays, n));
-        Integer cheap = hotelCosts.keySet().stream().min(Integer::compare).get();
-        hotelCosts.forEach((k, v) -> {
-            if (k == cheap)
-                sortedHotelCosts.put(k, v);
-        });
-        return sortedHotelCosts;
-    }
+	public Map<Integer, Hotel> getCheapestHotels(int weekDays, int weekendDays) {
+		Map<Integer, Hotel> hotelCosts = new HashMap<>();
+		Map<Integer, Hotel> sortedHotelCosts = new HashMap<>();
+		if (hotels.size() == 0)
+			return null;
+		this.hotels.stream().forEach(
+				n -> hotelCosts.put(n.getRegularWeekdayRate() * weekDays + n.getRegularWeekendRate() * weekendDays, n));
+		Integer cheap = hotelCosts.keySet().stream().min(Integer::compare).get();
+		hotelCosts.forEach((k, v) -> {
+			if (k == cheap)
+				sortedHotelCosts.put(k, v);
+		});
+		return sortedHotelCosts;
+	}
 
-    public int countTotalDays(String date1, String date2) {
+	public int countTotalDays(String date1, String date2) {
 
-        LocalDate startDate = toLocalDate(date1);
-        LocalDate endDate = toLocalDate(date2);
-        int totalDays = Period.between(startDate, endDate).getDays() + 1;
-        return totalDays;
-    }
+		LocalDate startDate = toLocalDate(date1);
+		LocalDate endDate = toLocalDate(date2);
+		int totalDays = Period.between(startDate, endDate).getDays() + 1;
+		return totalDays;
+	}
 
-    public int countWeekDays(String date1, String date2)
-    {
+	public int countWeekDays(String date1, String date2) {
 
-        LocalDate startDate = toLocalDate(date1);
-        LocalDate endDate = toLocalDate(date2);
+		LocalDate startDate = toLocalDate(date1);
+		LocalDate endDate = toLocalDate(date2);
 
-        DayOfWeek startDay = startDate.getDayOfWeek();
-        DayOfWeek endDay = endDate.getDayOfWeek();
+		DayOfWeek startDay = startDate.getDayOfWeek();
+		DayOfWeek endDay = endDate.getDayOfWeek();
 
-        long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
-        long daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
-        int totalWeekDays = (int) daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0)
-                + (endDay == DayOfWeek.SUNDAY ? 1 : 0);
+		long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
+		long daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
+		int totalWeekDays = (int) daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0)
+				+ (endDay == DayOfWeek.SUNDAY ? 1 : 0);
 
-        return totalWeekDays;
-    }
+		return totalWeekDays;
+	}
 
-    public LocalDate toLocalDate(String date)
-    {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
-        LocalDate localDate = LocalDate.parse(date, formatter);
-        return localDate;
-    }
+	public LocalDate toLocalDate(String date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		LocalDate localDate = LocalDate.parse(date, formatter);
+		return localDate;
+	}
 }
--- src/test/java/TestHotelReservationSystem.java
+++ src/test/java/TestHotelReservationSystem.java
@@ -7,34 +7,49 @@ import org.junit.Test;
 import com.bridgelabz.hotelreservationstsyem.Hotel;
 import com.bridgelabz.hotelreservationstsyem.HotelReservationSystem;
 
-	public class TestHotelReservationSystem {
+public class TestHotelReservationSystem {
 
-	    @Test
-	    public void whenHotelAdded_ToSystem_ShouldGetAdded() {
-	        Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
-	        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-	        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
-	        Hotel[] hotelList = {hotel1, hotel2, hotel3};
-	        List<Hotel> hotels = Arrays.asList(hotelList);
-	        HotelReservationSystem hotelReservation = new HotelReservationSystem();
-	        hotelReservation.add(hotel1);
-	        hotelReservation.add(hotel2);
-	        hotelReservation.add(hotel3);
-	        List<Hotel> result = hotelReservation.getHotelList();
-	        assertEquals(hotels, result);
-	    }
-	    @Test
-	    public void whenGivenDateRangeShouldReturnCheapestHotel()
-	    {
-	        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-	        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-	        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
-	        HotelReservationSystem hotelReservation = new HotelReservationSystem();
-	        hotelReservation.add(hotel1);
-	        hotelReservation.add(hotel2);
-	        hotelReservation.add(hotel3);
-	        Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
-	        result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
-	        assertNotNull(result);
-	    }
+	@Test
+	public void whenHotelAdded_ToSystem_ShouldGetAdded() {
+		Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel[] hotelList = { hotel1, hotel2, hotel3 };
+		List<Hotel> hotels = Arrays.asList(hotelList);
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		List<Hotel> result = hotelReservation.getHotelList();
+		assertEquals(hotels, result);
 	}
+
+	@Test
+	public void whenGivenDateRange_ShouldReturn_CheapestHotel() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+		result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+		assertNotNull(result);
+	}
+
+	@Test
+	public void whenGivenHotelAdded_ShouldAddWeekendPrices() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservationSystem hotelReservation = new HotelReservationSystem();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		List<Hotel> hotelList = hotelReservation.getHotelList();
+		boolean result = hotelList.get(0).getRegularWeekendRate() == 90
+				&& hotelList.get(1).getRegularWeekendRate() == 60 && hotelList.get(2).getRegularWeekendRate() == 150;
+		assertTrue(result);
+	}
+}
